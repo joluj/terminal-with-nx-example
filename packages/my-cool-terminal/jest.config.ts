20d3c77f59c44a1a3f3b8a993ef0860a6dc1cd2a
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2022-05-04 19:21:17 +0200

    init node and app setup

---
+++ packages/my-cool-terminal/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'my-cool-terminal',
+  preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/my-cool-terminal',
+};
