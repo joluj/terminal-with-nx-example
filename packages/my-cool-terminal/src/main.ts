3a3d00d86933f4e70945991517dfd8f705827448
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2022-05-04 21:16:18 +0200

    Add Vorpal

--- packages/my-cool-terminal/src/main.ts
+++ packages/my-cool-terminal/src/main.ts
@@ -1,8 +1,38 @@
-import { MyCoolClass } from './app';
-import { environment } from './environments/environment';
+import * as _vorpal from 'vorpal';
 
-console.log('Hello World! :D');
+const vorpal = new _vorpal();
 
-const cool = new MyCoolClass(environment.production);
+vorpal
+  .command('upload-nft')
+  .option('-n, --name name')
+  .action(async function (args, callback) {
+    const upload = (name) => {
+      this.log(`Uploading ${name}...`);
+      setTimeout(() => this.log('...'), 1000);
+      setTimeout(() => this.log('...'), 2000);
+      setTimeout(() => this.log('...'), 3000);
+      setTimeout(() => {
+        this.log('Done');
+        callback();
+      }, 4000);
+    };
 
-console.log(cool.doSomething());
+    if (args.options.name) {
+      upload(args.options.name);
+      return;
+    }
+
+    this.prompt(
+      {
+        type: 'input',
+        name: 'name',
+        message: 'How should it be called? ',
+      },
+      (result) => {
+        upload(result.name);
+        callback();
+      }
+    );
+  });
+
+vorpal.delimiter('my-cool-app $').show();
